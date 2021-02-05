dbcfb430d2787ff77b17a01e4dc046a707678740
sairaj <sairajjogu745@gmail.com>
2021-02-05 22:58:51 +0530

    College information

---
+++ College/College.java
@@ -0,0 +1,14 @@
+package org.details;
+public class College{
+    private void collegeId(){
+   System.out.println("college id is:45678");
+}
+  private void collegeName(){
+    System.out.println("college name is hindustan university");
+}
+    public static void main(String[]args){
+    College c = new College();
+     c.collegeId();
+c.collegeName();
+}
+}
